ec186383df0d4f875694484163587423da409705
Ario <zxdy@vip.qq.com>
2014-05-01 15:12:27 +0800

    ver2

--- force_brute_path.py
+++ force_brute_path.py
@@ -4,46 +4,37 @@
 
 import urllib2
 import socket
+import socks
 
 
-def assign(service, arg):
-    if service == "path":
-        return True, arg
+def set_socks5_proxy():
+    socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 1080)
+    socket.socket = socks.socksocket
 
-def audit(arg,file):
-    url = arg
+
+def set_http_proxy():
+    proxy_addr='218.64.255.253:3128'
+    proxy_handler = urllib2.ProxyHandler({'http':proxy_addr})
+    opener = urllib2.build_opener(proxy_handler)
+    urllib2.install_opener(opener)
+
+def audit(host,file,proxy):
+    if proxy=='socks5':
+        set_socks5_proxy()
+    elif proxy=='http':
+        set_http_proxy()
     path_list=read_dict(file)
-    # socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 1080)
-    # socket.socket = socks.socksocket
     for path in path_list:
-        request = urllib2.Request(url+path)
+        request = urllib2.Request(host+path)
         request.add_header('User-Agent', 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)')
         try:
             response = urllib2.urlopen(request,timeout=10)
             code=response.getcode()
             if code==200:
-                print(url+path)
+                print(host+path)
         except urllib2.URLError,e:
-            print e
-        except socket.timeout,e:
-            print type(e)
-
-def audit_with_http_proxy(arg,file):
-    url = arg
-    path_list=read_dict(file)
-    proxy_addr='218.64.255.253:3128'
-    proxy_handler = urllib2.ProxyHandler({'http':proxy_addr})
-    opener = urllib2.build_opener(proxy_handler)
-    urllib2.install_opener(opener)
-    for path in path_list:
-        request = urllib2.Request(url+path)
-        request.add_header('User-Agent', 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)')
-        try:
-            f = urllib2.urlopen(request,timeout=10)
-            if f:
-                print(url+path)
-        except urllib2.URLError,e:
-            print "."
+            # print '.'
+            pass
         except socket.timeout,e:
             print type(e)
 
@@ -55,5 +46,7 @@ def read_dict(file):
     return lines
 
 if __name__ == '__main__':
-    audit(assign('path', 'http://localhost')[1],'asp.txt')
-    # audit_with_http_proxy(assign('path', 'http://localhost')[1],'asp.txt')
+    host="http://localhost/"
+    dict="asp.txt"
+    proxy='http'
+    audit(host,dict,proxy)
