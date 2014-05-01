de4a61df6dde8186d6d62a0a8f7858f015a3aae0
Ario <zxdy@vip.qq.com>
2014-05-01 16:58:55 +0800

    add usage

--- force_brute_path.py
+++ force_brute_path.py
@@ -2,18 +2,24 @@
 #-*- coding: utf-8 -*-
 #__author__ = 'Ario'
 
+from optparse import OptionParser
 import urllib2
 import socket
 import socks
 
-
 def set_socks5_proxy():
-    socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 1080)
+    '''
+    set the socks5 proxy
+    '''
+    socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 1080) #todo: read from a proxy list file
     socket.socket = socks.socksocket
 
 
 def set_http_proxy():
-    proxy_addr='218.64.255.253:3128'
+    '''
+    set the http proxy
+    '''
+    proxy_addr='218.64.255.253:3128' #todo:read from a proxy list file
     proxy_handler = urllib2.ProxyHandler({'http':proxy_addr})
     opener = urllib2.build_opener(proxy_handler)
     urllib2.install_opener(opener)
@@ -31,12 +37,12 @@ def audit(host,file,proxy):
             response = urllib2.urlopen(request,timeout=10)
             code=response.getcode()
             if code==200:
-                print(host+path)
+                print(host+path) #todo:write log,save the successful results
         except urllib2.URLError,e:
             # print '.'
             pass
         except socket.timeout,e:
-            print type(e)
+            print type(e)  #todo:try another proxy if the current proxy tunnel is timeout
 
 def read_dict(file):
     lines=[]
@@ -45,8 +51,16 @@ def read_dict(file):
             lines.append(line)
     return lines
 
+def main():
+    options = OptionParser(usage='%prog url [options]', description='Test for path brute force attack')
+    options.add_option('-d', '--dict', type='string', default='php.txt', help='dictionary of path for using')
+    options.add_option('-p', '--proxy', type='string', default='http', help='proxy type:http,socks5')
+    opts, args = options.parse_args()
+    if len(args) < 1:
+        options.print_help()
+        return
+    audit(args[0],opts.dict,opts.proxy)
+
+
 if __name__ == '__main__':
-    host="http://localhost/"
-    dict="asp.txt"
-    proxy='http'
-    audit(host,dict,proxy)
+    main()
